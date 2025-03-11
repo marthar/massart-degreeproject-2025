24bdd5d6c9b73d14b3ee8832dd3ee79d85e46468
Pascal Rettig <pascal@cykod.com>
2025-03-11 11:33:07 -0400

    Updates

---
+++ precache-manifest.9e2e91d2eaf1b9d4e7521495ad4f1ea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf06cba7fb0d445f2765bd7edca21e5",
+    "url": "/massart-degreeproject-2022/index.html"
+  },
+  {
+    "revision": "9342ea935daf80b3bd90",
+    "url": "/massart-degreeproject-2022/static/css/main.66e78aee.chunk.css"
+  },
+  {
+    "revision": "7a4910a5e946290e33fc",
+    "url": "/massart-degreeproject-2022/static/js/2.f93bdbdf.chunk.js"
+  },
+  {
+    "revision": "fd0d82301d7744a290e71cebc7c7219e",
+    "url": "/massart-degreeproject-2022/static/js/2.f93bdbdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9342ea935daf80b3bd90",
+    "url": "/massart-degreeproject-2022/static/js/main.d77982e5.chunk.js"
+  },
+  {
+    "revision": "30001dfd76ed0f7f5d60",
+    "url": "/massart-degreeproject-2022/static/js/runtime-main.e05ade5c.js"
+  }
+]);
